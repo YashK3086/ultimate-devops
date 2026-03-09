e550cf2ebae8ac902c680e5fe0edcf91bd296b5b
Yashvardhan Khanna <yashvardhankhanna360@gmail.com>
2026-03-09 07:32:53 +0530

    Removed heavy binaries and fixed 100MB limit

--- terraform-v3/main.tf
+++ terraform-v3/main.tf
@@ -1,30 +1,43 @@
-# terraform-v3/main.tf
 provider "aws" {
   region = "us-east-1"
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  name   = "devops-assignment-5-vpc"
-  cidr   = "10.0.0.0/16"
-  azs    = ["us-east-1a", "us-east-1b"]
-  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "devops-assignment-5-vpc"
+  cidr = "10.0.0.0/16"
+  azs  = ["us-east-1a", "us-east-1b"]
+
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.10.0/24", "10.0.11.0/24"]
+
+  # ADD THESE TWO LINES TO FIX THE ERROR:
+  map_public_ip_on_launch = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
   cluster_name    = "devops-cluster-v5"
   cluster_version = "1.31"
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.public_subnets
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.public_subnets
+  cluster_endpoint_public_access = true
+
   enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     nodes = {
       min_size     = 1
-      max_size     = 2
+      max_size     = 1  # Reduce this to save resources
       desired_size = 1
-      instance_types = ["t3.medium"]
+      instance_types = ["t3.micro"] # CHANGE THIS FROM t3.medium
     }
   }
 }
